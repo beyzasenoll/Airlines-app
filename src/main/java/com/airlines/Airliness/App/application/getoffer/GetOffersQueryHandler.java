a4763129e2a89ae95f68dd285cbc7df5985303e8
beyzasenoll <beyzasenol08@gmail.com>
2024-11-27 17:47:51 +0300

    completed handle method

--- src/main/java/com/airlines/Airliness/App/application/getoffer/GetOffersQueryHandler.java
+++ src/main/java/com/airlines/Airliness/App/application/getoffer/GetOffersQueryHandler.java
@@ -1,8 +1,11 @@
 package com.airlines.Airliness.App.application.getoffer;
 
+import com.airlines.Airliness.App.domain.model.Offer;
+import com.airlines.Airliness.App.domain.model.OfferFilter;
 import com.airlines.Airliness.App.domain.port.OfferPort;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GetOffersQueryHandler {
     private List<OfferPort> offerPortList;
@@ -12,8 +15,28 @@ public class GetOffersQueryHandler {
     }
 
     public GetOffersQueryResponse handle(GetOffersQuery getOffersQuery) {
-        //offerPortList.stream().map(offerPort -> {
-        //    offerPort.getOffer()
-        //});
+
+        OfferFilter offerFilter = createOfferFilter(getOffersQuery);
+        List<Offer> offers=getOffersFromPorts(offerFilter);
+
+        GetOffersQueryResponse getOffersQueryResponse = new GetOffersQueryResponse();
+        getOffersQueryResponse.setOfferList(offers);
+
+        return  getOffersQueryResponse;
+    }
+
+    private OfferFilter createOfferFilter(GetOffersQuery getOffersQuery) {
+        return OfferFilter.builder()
+                .departureAirport(getOffersQuery.getDepartureAirport())
+                .arrivalAirport(getOffersQuery.getArrivalAirport())
+                .departureDate(getOffersQuery.getDepartureDate())
+                .returnDate(getOffersQuery.getReturnDate())
+                .build();
+    }
+    private List<Offer> getOffersFromPorts(OfferFilter offerFilter) {
+        return offerPortList.stream()
+                .map(offerPort -> offerPort.getOffer(offerFilter))
+                .filter(offer -> offer != null)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/airlines/Airliness/App/application/getoffer/GetOffersQueryResponse.java
+++ src/main/java/com/airlines/Airliness/App/application/getoffer/GetOffersQueryResponse.java
@@ -1,4 +1,11 @@
 package com.airlines.Airliness.App.application.getoffer;
 
+import com.airlines.Airliness.App.domain.model.Offer;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class GetOffersQueryResponse {
+    List<Offer> offerList;
 }
